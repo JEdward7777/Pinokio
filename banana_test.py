e3401957ff46f3da3cefb7f06ff9c4d53725176c
Joshua <HootOwl7777+git@gmail.com>
2018-10-27 14:21:43 -0400

    bababa?  Haven't heard of that package

--- banana_test.py
+++ banana_test.py
@@ -3,7 +3,7 @@
 #https://stackoverflow.com/questions/45068568/is-it-possible-to-create-a-new-gym-environment-in-openai
 
 import gym
-import gym_bababa
+import gym_banana
 env = gym.make('Banana-v0')
 env.num_envs = 1
 learn = r.get_learn_function("ppo2")
